e4070adfa120eec243099c2f398358458728d911
Sharvin Vincent <sharvinvincent9100@gmail.com>
2026-04-21 22:16:37 +0530

    Basic codes more

--- removeduplicatesandsort.py
+++ removeduplicatesandsort.py
@@ -1,9 +1,12 @@
 arr = [1,1,2,2,2,3,3]
 i = 0
-j = 1
-for j in range(len(arr)):
+for j in range(1,len(arr)):
     if arr[j] != arr[i]:
-        arr[i+1] = arr[j]
         i += 1
-print(arr[:i+1])
+        arr[i] = arr[j]
+for k in range(i+1,len(arr)):
+    arr[k] = '_'
+print(arr)
+
+
         
