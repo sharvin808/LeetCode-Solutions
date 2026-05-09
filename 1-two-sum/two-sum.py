71b8c35d26d1eccb9893aed8042be2d3e6342c57
Sharvin Vincent <sharvinvincent9100@gmail.com>
2026-05-09 10:56:16 +0530

    sort0s1s2s

--- 1-two-sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, num: List[int], target: int) -> List[int]:
-        seen = {}
-        for i,nums in enumerate(num):
-            complement = target - nums
-            if complement in seen:
-                return(seen[complement],i)
-            seen[nums] = i
-
